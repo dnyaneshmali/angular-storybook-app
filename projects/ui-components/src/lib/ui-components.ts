94dd48dface59d09f573d2eb3f8028988a6972a8
Sanket <sanket7575757575@gmail.com>
2026-07-29 23:11:15 +0530

    initial commit

---
+++ projects/ui-components/src/lib/ui-components.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ui-components',
+  imports: [],
+  template: ` <p>ui-components works!</p> `,
+  styles: ``,
+})
+export class UiComponents {}
